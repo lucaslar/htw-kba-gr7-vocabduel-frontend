7641694a09e688b8a9496b8900ebe6eb2219781c
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-17 14:32:41 +0200

    Implemented basic login.

--- src/app/components/main/login/login.component.ts
+++ src/app/components/main/login/login.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { LoginData } from '../../../model/internal/login-data';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class LoginComponent {
+    data: LoginData = { email: '', password: '' };
+    constructor(readonly auth: AuthService) {}
+    // TODO Error handling
 }
