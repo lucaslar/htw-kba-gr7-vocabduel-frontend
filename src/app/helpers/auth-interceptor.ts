f84106246906437b14f40cac719e21a5f776c2c8
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-17 12:36:54 +0200

    Added auth interceptor.

---
+++ src/app/helpers/auth-interceptor.ts
@@ -0,0 +1,89 @@
+import { Injectable } from '@angular/core';
+import {
+    HttpErrorResponse,
+    HttpEvent,
+    HttpHandler,
+    HttpInterceptor,
+    HttpRequest,
+} from '@angular/common/http';
+import { StorageService } from '../services/storage.service';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { catchError, filter, switchMap, take } from 'rxjs/operators';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+    private readonly tokenHeader = 'x-access-token';
+    private refreshingInProgress = false;
+    private readonly accessTokenSubject: BehaviorSubject<
+        string | null
+    > = new BehaviorSubject<string | null>(null);
+
+    constructor(
+        private readonly storage: StorageService,
+        private readonly auth: AuthService
+    ) {}
+
+    intercept(
+        req: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        const token = this.storage.token;
+        return next.handle(this.appendAuthHeader(req, token)).pipe(
+            catchError((err) => {
+                if (err instanceof HttpErrorResponse && err.status === 401) {
+                    return this.storage.refreshToken && token
+                        ? this.refreshToken$(req, next)
+                        : this.logoutAndThrow(err);
+                } else if (
+                    err instanceof HttpErrorResponse &&
+                    err.status === 403
+                ) {
+                    return this.logoutAndThrow(err);
+                } else return throwError(err);
+            })
+        );
+    }
+
+    private appendAuthHeader(
+        req: HttpRequest<any>,
+        token: string | null
+    ): HttpRequest<any> {
+        return token
+            ? req.clone({ headers: req.headers.set(this.tokenHeader, token) })
+            : req;
+    }
+
+    private logoutAndThrow(err: any): Observable<HttpEvent<any>> {
+        this.auth.logout();
+        return throwError(err);
+    }
+
+    private refreshToken$(
+        request: HttpRequest<any>,
+        next: HttpHandler
+    ): Observable<HttpEvent<any>> {
+        if (!this.refreshingInProgress) {
+            this.refreshingInProgress = true;
+            this.accessTokenSubject.next(null);
+
+            return this.auth.refreshToken$.pipe(
+                switchMap((res) => {
+                    this.refreshingInProgress = false;
+                    this.accessTokenSubject.next(res.token);
+                    return next.handle(
+                        this.appendAuthHeader(request, res.token)
+                    );
+                })
+            );
+        } else {
+            return this.accessTokenSubject.pipe(
+                filter((token) => token !== null),
+                take(1),
+                switchMap((token) => {
+                    return next.handle(this.appendAuthHeader(request, token));
+                })
+            );
+        }
+    }
+}
