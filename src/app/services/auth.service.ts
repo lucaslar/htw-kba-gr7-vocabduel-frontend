d65b88fae37fd5fc1fb4780d6d744109dab67acc
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-17 12:22:30 +0200

    Implemented core authentication logic.

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { LoginData } from '../model/internal/login-data';
+import { StorageService } from './storage.service';
+
+// TODO move to own files
+
+export class ReturnedType {
+    readonly token: string = '';
+    readonly refreshToken: string = '';
+    readonly userData: any;
+}
+
+export class TokenResponse {
+    readonly token: string = '';
+    readonly refreshToken: string = '';
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthService {
+    private readonly user$ = new BehaviorSubject<ReturnedType | null>(null);
+
+    constructor(
+        private readonly http: HttpClient,
+        private readonly storage: StorageService,
+        private readonly jwtHelper: JwtHelperService,
+        private readonly router: Router
+    ) {}
+
+    // TODO Implement procedure after registration/login
+
+    login(loginData: LoginData): void {
+        const url = `${environment.endpointUrl}/login`;
+        this.http
+            .post<ReturnedType>(url, loginData)
+            .subscribe((result) => this.onSuccessfulAuth(result));
+    }
+
+    // TODO: Typify
+    register(userData: any): void {
+        const url = `${environment.endpointUrl}/register`;
+        this.http
+            .post<ReturnedType>(url, userData)
+            .subscribe((result) => this.onSuccessfulAuth(result));
+    }
+
+    get refreshToken$(): Observable<TokenResponse> {
+        const refreshToken = this.storage.refreshToken;
+        const url = `${environment.endpointUrl}/refresh-token`;
+        return this.http.post<TokenResponse>(url, { refreshToken }).pipe(
+            tap((response) => {
+                this.storage.token = response.token;
+                this.storage.refreshToken = response.refreshToken;
+            })
+        );
+    }
+
+    get currentUser$(): Observable<ReturnedType | null> {
+        return this.user$.pipe(
+            switchMap((user) => {
+                if (user) return of(user);
+                else if (this.storage.token) return this.fetchCurrentUser$;
+                else return of(null);
+            })
+        );
+    }
+
+    logout(): void {
+        this.storage.token = null;
+        this.storage.refreshToken = null;
+        this.router.navigate(['login']).then();
+        this.user$.next(null);
+    }
+
+    private get fetchCurrentUser$(): Observable<ReturnedType> {
+        return this.http
+            .get<ReturnedType>(`${environment.endpointUrl}/current-user`)
+            .pipe(
+                tap((user) => {
+                    if (!user) this.storage.token = null;
+                    this.user$.next(user.userData);
+                })
+            );
+    }
+
+    private onSuccessfulAuth(result: ReturnedType): void {
+        this.storage.token = result.token;
+        this.storage.refreshToken = result.refreshToken;
+        console.log(result.userData);
+        this.user$.next(result.userData);
+        this.router.navigate(['dashboard']).then();
+    }
+}
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class StorageService {
+    private readonly authTokenKey = 'authToken';
+    private readonly refreshTokenKey = 'refreshToken';
+    private _token: string | null = null;
+    private _refreshToken: string | null = null;
+
+    get token(): string | null {
+        if (!this._token) {
+            this._token = sessionStorage.getItem(this.authTokenKey);
+        }
+        return this._token;
+    }
+
+    set token(value: string | null) {
+        this._token = value;
+        if (value) sessionStorage.setItem(this.authTokenKey, value);
+        else sessionStorage.removeItem(this.authTokenKey);
+    }
+
+    get refreshToken(): string | null {
+        if (!this._refreshToken) {
+            this._refreshToken = sessionStorage.getItem(this.refreshTokenKey);
+        }
+        return this._refreshToken;
+    }
+
+    set refreshToken(value: string | null) {
+        this._refreshToken = value;
+        if (value) sessionStorage.setItem(this.refreshTokenKey, value);
+        else sessionStorage.removeItem(this.refreshTokenKey);
+    }
+}
