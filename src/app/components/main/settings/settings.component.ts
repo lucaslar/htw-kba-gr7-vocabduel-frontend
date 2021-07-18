f49463538526d8383a071770a59101551fe5ff6e
Lucas Larisch <lucas.larisch@googlemail.com>
2021-07-18 13:12:22 +0200

    Snackbar after setting endpoint URL.

--- src/app/components/main/settings/settings.component.ts
+++ src/app/components/main/settings/settings.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { AuthService } from '../../../services/auth.service';
 import { StorageService } from '../../../services/storage.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-settings',
@@ -11,5 +13,18 @@ import { StorageService } from '../../../services/storage.service';
 export class SettingsComponent {
     readonly defaultEndpoint = environment.endpointUrl;
 
-    constructor(readonly auth: AuthService, readonly storage: StorageService) {}
+    constructor(
+        readonly auth: AuthService,
+        readonly storage: StorageService,
+        private readonly snackbar: MatSnackBar,
+        private readonly translate: TranslateService
+    ) {}
+
+    showSnackbar(messageKey: string): void {
+        this.snackbar.open(
+            this.translate.instant(messageKey),
+            this.translate.instant('general.close'),
+            { duration: 3000 }
+        );
+    }
 }
